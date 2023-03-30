42e35a4db69c45d2a3fbdbc138b3b1c657e3532f
urvi bajaj <urvibajaj9871@gmail.com>
2023-03-30 12:17:31 +0530

    changes

--- Login/login.aspx.cs
+++ Login/login.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Reflection.Emit;
 
 namespace Login
 {
@@ -21,7 +25,28 @@ namespace Login
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["dbconnection"].ToString());
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from logindb where username='"txtUsername.Text+"' and password ='"+txtPassword.Text"');
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    Label4.Text = "Login Sucess......!!";
+                }
+                else
+                {
+                    Label4.Text = "UserId & Password Is not correct Try again..!!";
 
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
         }
     }
 }
